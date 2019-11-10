dea3915e204dbc00b18aa97237564bc83c8529e6
Allen Anthes <abanthes@gmail.com>
2019-11-10 14:26:36 -0600

    message

--- TheHuddleSite/src/main/java/com/javalec/spring/TheHuddleSite/controller/TemplateController.java
+++ TheHuddleSite/src/main/java/com/javalec/spring/TheHuddleSite/controller/TemplateController.java
@@ -1,10 +1,11 @@
 package com.javalec.spring.TheHuddleSite.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
 public class TemplateController {
@@ -17,5 +18,16 @@ public class TemplateController {
     @GetMapping("/main")
     public String getMain() {
         return "main";
+    }
+
+    @GetMapping("/signup")
+    public String getSignup() {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String postSignup(@RequestBody Map<String, Object> body) {
+        System.out.println(body);
+        return "signup";
     }
 }

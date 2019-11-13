0760cdb848dc9a508749db28f58328f61fdbfbfc
GeekVader720 <49813301+GeekVader720@users.noreply.github.com>
2019-11-13 14:20:20 -0600

    Good

--- TheHuddleSite/src/main/java/com/javalec/spring/TheHuddleSite/controller/TemplateController.java
+++ TheHuddleSite/src/main/java/com/javalec/spring/TheHuddleSite/controller/TemplateController.java
@@ -1,33 +1,58 @@
 package com.javalec.spring.TheHuddleSite.controller;
 
+import com.javalec.spring.TheHuddleSite.model.User;
+import com.javalec.spring.TheHuddleSite.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
 
 @Controller
 public class TemplateController {
 
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public TemplateController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
     @GetMapping("/login")
     public String getLogin() {
         return "login";
     }
 
-    @GetMapping("/main")
-    public String getMain() {
-        return "main";
+    @GetMapping
+    public String getRoot() {
+        return "login";
     }
+
+    @GetMapping("/main")
+    public String getMain() { return "main"; }
+
+    @GetMapping("/chatroom")
+    public String getChat() { return "chatroom"; }
 
     @GetMapping("/signup")
-    public String getSignup() {
-        return "signup";
+    public String getSignup() { return "signup"; }
+
+    @PostMapping(value = "/signup")
+    public RedirectView postSignup(@ModelAttribute User user) {
+        userRepository.save(user);
+        return new RedirectView("login");
     }
 
-    @PostMapping("/signup")
-    public String postSignup(@RequestBody Map<String, Object> body) {
-        System.out.println(body);
-        return "signup";
+    @PostMapping(value = "/login")
+    public RedirectView postLogin(@RequestBody Map<String, Object> body) {
+        return new RedirectView("login");
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody User getUser() {
+        return userRepository.findAll().get(0);
     }
 }
